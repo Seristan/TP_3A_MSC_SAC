1b95895db9e47d385e2e07fa39b8c9012f290603
Yac-Spr <yacinespringer@gmail.com>
2024-09-19 12:23:46 +0200

    TP1

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -6,7 +6,7 @@
   ******************************************************************************
   * @attention
   *
-  * Copyright (c) 2024 STMicroelectronics.
+  * Copyright (c) 2022 STMicroelectronics.
   * All rights reserved.
   *
   * This software is licensed under terms that can be found in the LICENSE file
@@ -25,39 +25,99 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include <string.h>
+#include <stdio.h>
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN PTD */
-
 /* USER CODE END PTD */
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
 
+#define UART_TX_BUFFER_SIZE 64 /**< Taille du buffer d'émission UART */
+#define UART_RX_BUFFER_SIZE 1  /**< Taille du buffer de réception UART */
+#define CMD_BUFFER_SIZE     64 /**< Taille du buffer de commande */
+#define MAX_ARGS            9  /**< Nombre maximum d'arguments */
+
+/** Codes ASCII */
+
+#define ASCII_LF  0x0A /**< Line Feed (saut de ligne) */
+#define ASCII_CR  0x0D /**< Carriage Return (retour chariot) */
+#define ASCII_DEL 0x7F /**< Delete (suppression) */
+
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
 /* USER CODE BEGIN PM */
-
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
 
 /* USER CODE BEGIN PV */
 
+/** @brief Prompt affiché */
+const uint8_t prompt[] = "user@Nucleo-STM32G431>>";
+
+/** @brief Message de bienvenue au démarrage du microprocesseur */
+const uint8_t started[] =
+    "\r\n*-----------------------------*"
+    "\r\n| Welcome on Nucleo-STM32G431 |"
+    "\r\n*-----------------------------*"
+    "\r\n";
+
+/** @brief chaîne pour le retour à la ligne */
+const uint8_t newLine[] = "\r\n";
+
+/** @brief Message d'aide listant les fonctions disponibles */
+const uint8_t helpMsg[] =
+    "Available commands:\r\n"
+    "help    : Display this help message\r\n"
+    "pinout  : Display the list of used pins\r\n"
+    "start   : Power ON the motor stage\r\n"
+    "stop    : Power OFF the motor stage\r\n";
+
+/** @brief Liste des broches utilisées */
+const uint8_t pinout[] =
+    "Used pins:\r\n"
+    "PA5 : LED\r\n"
+    "PA2 : USART2_TX\r\n"
+    "PA3 : USART2_RX\r\n";
+
+/** @brief Message indiquant l'allumage du moteur */
+const uint8_t powerOn[] = "Power ON\r\n";
+
+/** @brief Message indiquant l'extinction du moteur */
+const uint8_t powerOff[] = "Power OFF\r\n";
+
+/** @brief Message pour commande non reconnue */
+const uint8_t cmdNotFound[] = "Command not found\r\n";
+
+/** @brief Flag indiquant la réception d'un caractère sur l'UART */
+uint32_t uartRxReceived;
+
+/** @brief Buffer de réception UART */
+uint8_t uartRxBuffer[UART_RX_BUFFER_SIZE];
+
+/** @brief Buffer d'émission UART */
+uint8_t uartTxBuffer[UART_TX_BUFFER_SIZE];
+
+/** @brief Buffer contenant la commande en cours */
+char cmd[CMD_BUFFER_SIZE];
+
+/** @brief Index pour le prochain caractère à remplir */
+int idxCmd;
+
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 /* USER CODE BEGIN PFP */
-
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-
 /* USER CODE END 0 */
 
 /**
@@ -68,6 +128,17 @@ int main(void)
 {
 
   /* USER CODE BEGIN 1 */
+  /** @brief Tableau des arguments extraits de la commande */
+  char* argv[MAX_ARGS];
+
+  /** @brief Nombre d'arguments */
+  int argc = 0;
+
+  /** @brief Token pour l'analyse de la chaîne */
+  char* token;
+
+  /** @brief Flag indiquant qu'une nouvelle commande est prête */
+  int newCmdReady = 0;
 
   /* USER CODE END 1 */
 
@@ -77,7 +148,11 @@ int main(void)
   HAL_Init();
 
   /* USER CODE BEGIN Init */
-
+  idxCmd = 0;
+  memset(cmd, '\0', CMD_BUFFER_SIZE);
+  memset(argv, 0, MAX_ARGS * sizeof(char*));
+  memset(uartRxBuffer, 0, UART_RX_BUFFER_SIZE);
+  memset(uartTxBuffer, 0, UART_TX_BUFFER_SIZE);
   /* USER CODE END Init */
 
   /* Configure the system clock */
@@ -97,19 +172,136 @@ int main(void)
   MX_USART3_UART_Init();
   /* USER CODE BEGIN 2 */
 
+  HAL_UART_Receive_IT(&huart2, uartRxBuffer, UART_RX_BUFFER_SIZE);
+  HAL_Delay(10);
+  HAL_UART_Transmit(&huart2, started, strlen((char*)started), HAL_MAX_DELAY);
+  HAL_UART_Transmit(&huart2, prompt, strlen((char*)prompt), HAL_MAX_DELAY);
+
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
+
   while (1)
   {
+	/**
+	   * @brief Vérifie si un caractère a été reçu sur l'UART.
+	   */
+    if (uartRxReceived)
+    {
+      uint8_t receivedChar = uartRxBuffer[0];
+      /**
+        * @brief Écho du caractère reçu sur l'UART.
+        */
+      HAL_UART_Transmit(&huart2, &receivedChar, 1, HAL_MAX_DELAY);
+
+      /**
+        * @brief Traitement du caractère reçu.
+        */
+      switch (receivedChar)
+      {
+      /**
+         * @brief Cas du retour chariot ou du saut de ligne : traite la commande entrée.
+         */
+        case ASCII_CR:
+        case ASCII_LF:
+          HAL_UART_Transmit(&huart2, newLine, strlen((char*)newLine), HAL_MAX_DELAY);
+          cmd[idxCmd] = '\0';
+          argc = 0;
+          token = strtok(cmd, " ");
+          while (token != NULL && argc < MAX_ARGS)
+          {
+            argv[argc++] = token;
+            token = strtok(NULL, " ");
+          }
+
+          idxCmd = 0;
+          newCmdReady = 1;
+          break;
+          /**
+              * @brief Cas de la suppression : efface le dernier caractère saisi.
+              */
+        case ASCII_DEL:
+          if (idxCmd > 0)
+          {
+            idxCmd--;
+            cmd[idxCmd] = '\0';
+            // Envoyer un retour en arrière pour effacer dans le terminal
+            uint8_t backspace = '\b';
+            HAL_UART_Transmit(&huart2, &backspace, 1, HAL_MAX_DELAY);
+          }
+          break;
+
+        /**
+           * @brief Cas par défaut : ajoute le caractère au buffer de commande.
+           */
+        default:
+          if (idxCmd < CMD_BUFFER_SIZE - 1)
+          {
+            cmd[idxCmd++] = receivedChar;
+          }
+          else
+          {
+
+            idxCmd = 0;
+            memset(cmd, '\0', CMD_BUFFER_SIZE);
+            HAL_UART_Transmit(&huart2, (uint8_t*)"Command too long\r\n", 18, HAL_MAX_DELAY);
+            HAL_UART_Transmit(&huart2, prompt, strlen((char*)prompt), HAL_MAX_DELAY);
+          }
+          break;
+      }
+      uartRxReceived = 0;
+      /**
+        * @brief Relance la réception UART en interruption.
+        */
+      HAL_UART_Receive_IT(&huart2, uartRxBuffer, UART_RX_BUFFER_SIZE);
+    }
+    /**
+      * @brief Vérifie si une nouvelle commande est prête à être traitée.
+      */
+    if (newCmdReady)
+    {
+      newCmdReady = 0;
+      if (argc > 0)
+      {
+    	/**
+    	  * @brief (Sera remplacé par une fonction) Traite les commandes reconnues : help, pinout, start, stop.
+    	  */
+        if (strcmp(argv[0], "help") == 0)
+        {
+          HAL_UART_Transmit(&huart2, helpMsg, strlen((char*)helpMsg), HAL_MAX_DELAY);
+        }
+        else if (strcmp(argv[0], "pinout") == 0)
+        {
+          HAL_UART_Transmit(&huart2, pinout, strlen((char*)pinout), HAL_MAX_DELAY);
+        }
+        else if (strcmp(argv[0], "start") == 0)
+        {
+          HAL_UART_Transmit(&huart2, powerOn, strlen((char*)powerOn), HAL_MAX_DELAY);
+        }
+        else if (strcmp(argv[0], "stop") == 0)
+        {
+          HAL_UART_Transmit(&huart2, powerOff, strlen((char*)powerOff), HAL_MAX_DELAY);
+        }
+        else
+        {
+          HAL_UART_Transmit(&huart2, cmdNotFound, strlen((char*)cmdNotFound), HAL_MAX_DELAY);
+        }
+      }
+      // Affiche le prompt
+      HAL_UART_Transmit(&huart2, prompt, strlen((char*)prompt), HAL_MAX_DELAY);
+
+      // Réinitialise le buffer de commande et l'index
+      idxCmd = 0;
+      memset(cmd, '\0', CMD_BUFFER_SIZE);
+    }
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-  }
-  /* USER CODE END 3 */
-}
 
+  /* USER CODE END 3 */
+ }
+}
 /**
   * @brief System Clock Configuration
   * @retval None
@@ -156,7 +348,19 @@ void SystemClock_Config(void)
 }
 
 /* USER CODE BEGIN 4 */
-
+/**
+  * @brief Callback appelé à la fin de la réception UART.
+  * @param huart Handle de l'UART.
+  * @retval None
+  */
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+{
+  if (huart->Instance == USART2)
+  {
+    uartRxReceived = 1;
+    // La relance de la réception UART est effectuée dans la boucle principale
+  }
+}
 /* USER CODE END 4 */
 
 /**
